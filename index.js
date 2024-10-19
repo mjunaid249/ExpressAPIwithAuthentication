42f331367e8d3502873052337ea7f53bc0917d8e
mjunaid249 <junaidch249249@gmail.com>
2024-10-19 09:15:31 -0700

    Fixed Cors Error

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import { connectDb } from "./data/database.js";
 import userRouter from "./routes/user.js";
 import cookieParser from "cookie-parser";
 import taskRouter from "./routes/task.js";
-
+import cors from "cors";
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 dotenv.config();
 const PORT = process.env.PORT || 2000;
 
@@ -12,6 +16,7 @@ const app = express();
 connectDb();
 
 //Middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
